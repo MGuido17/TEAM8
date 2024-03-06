2acf070647ebdbe591bb6a7abd5db206bd2dcce0
Nicolas Ottati <ottatinicolas91@gmail.com>
2024-03-06 22:01:40 +0100

    Added a method to delete attendance and de partial is now dynamic :)

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -10,4 +10,16 @@ class AttendeesController < ApplicationController
       redirect_to @activity, alert: "There's been an error while confirming your asistance. Please try again later."
     end
   end
+
+  def destroy
+    @activity = Activity.find(params[:activity_id])
+    @attendee = @activity.attendees.find_by(user_id: current_user.id)
+    
+    if @attendee.destroy
+      redirect_to @activity, notice: "Asistance removed"
+    else
+      redirect_to @activity, alert: 'Hubo un error al eliminar la asistencia.'
+    end
+  end
+
 end
