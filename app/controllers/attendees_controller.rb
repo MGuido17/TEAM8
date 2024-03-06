104aa3a0e9880ddf62ced6b943dae9de076fdd39
Nicolas Ottati <ottatinicolas91@gmail.com>
2024-03-06 20:43:56 +0100

    Added button to attend an event & it's functionallity. We're gonna use it in some other places so I'm creating a partial to re-use it :)

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,2 +1,13 @@
 class AttendeesController < ApplicationController
+  
+  def create
+    @activity = Activity.find(params[:activity_id])
+    @attendee = @activity.attendees.build(user: current_user)
+
+    if @attendee.save
+      redirect_to @activity, notice: "Asistance confirmed!"
+    else
+      redirect_to @activity, alert: "There's been an error while confirming your asistance. Please try again later."
+    end
+  end
 end
