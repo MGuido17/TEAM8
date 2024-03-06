cd2816f822124b4ef48ebac7e5e602ecc5172a65
Nicolas Ottati <ottatinicolas91@gmail.com>
2024-03-06 15:04:11 +0100

    created user controller, created 3 partials, updated routes, now linking everything...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def index
+    @activities = Activity.all
+  end
+
+  def show    
+  end
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :activities, only: [:index, :show, :new, :create]
+  end
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
