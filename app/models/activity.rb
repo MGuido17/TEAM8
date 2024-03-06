b57291f26efd154b6e13d3cb05400f1188e84c2c
valeria lezcano <msvalerialg@gmail.com>
2024-03-06 14:27:56 +0100

    added three columns to activites, changed model for activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,9 +1,8 @@
 class Activity < ApplicationRecord
   belongs_to :user
-  belongs_to :attendees
+  has_many :attendees
   has_many :bookmarks
   has_many :reviews
-  
   serialize :recommended_conditions, Array
   serialize :neutral_conditions, Array
   serialize :not_recommended_conditions, Array
