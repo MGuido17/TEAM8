a487ec5b5175ccac1c87ab2100a00dc34cc1170d
Nicolas Ottati <ottatinicolas91@gmail.com>
2024-03-07 22:48:15 +0100

    implemented the logic for activities_index, but again I think the seed file needs to be modified as we don't have a profile created yet, so at the moment Idk if it's actually working or not :)

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,20 @@
 class ActivitiesController < ApplicationController
+  before_action :set_user, only: :index
+
 
   def index
-    @activities = Activity.all
+    if @user
+      recommended_conditions = @user.profiles.pluck(:recommended_conditions).flatten
+      neutral_conditions = @user.profiles.pluck(:neutral_conditions).flatten
+      not_recommended_conditions = @user.profiles.pluck(:not_recommended_conditions).flatten
+
+      @activities = Activity.includes(:profiles).where.not(profiles: { not_recommended_conditions: not_recommended_conditions })
+                            .or(Activity.includes(:profiles).where(profiles: { recommended_conditions: recommended_conditions }))
+                            .or(Activity.includes(:profiles).where(profiles: { neutral_conditions: neutral_conditions }))
+    else
+      # Handle the case when @user is nil
+      @activities = Activity.all
+    end
   end
 
   def new
@@ -30,5 +43,9 @@ private
       whitelisted[:recommended_conditions] = params[:activity][:recommended_conditions].reject(&:blank?)
       whitelisted[:not_recommended_conditions] = params[:activity][:not_recommended_conditions].reject(&:blank?)
     end
+  end
+
+  def set_user
+    @user = current_user
   end
 end
