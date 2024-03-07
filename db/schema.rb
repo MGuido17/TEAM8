b2efcdd044ddc121499bde9640f736b2f883ef71
Francisco Posada <ciscotomaramo@gmail.com>
2024-03-07 15:30:18 +0100

    images folder recreated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_112821) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_113310) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
