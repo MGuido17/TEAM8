7096fc2aa5ba9c1250f9238eb2d0f9d465e79496
Martin Guido <m.guido.r@hotmail.com>
2024-03-07 11:36:22 +0100

    changed routes, and profile model to be destroy dependent on user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  resources :profiles
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: :show do
-    resources :activities, only: [:new, :create]
-  end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -15,10 +11,10 @@ Rails.application.routes.draw do
   # User routes defined
   resources :users, only: [:show, :index]
 
-  resources :activities, only: [:index, :show] do
+  resources :activities do
     resources :invites, only: [:create, :update] # Para invitar usuarios
     resources :attendees, only: [:create, :destroy] # Para unirse a actividades
   end
 
-  resources :profiles, except: [:index]
+  resources :profiles, except: [:index, :destroy]
 end
