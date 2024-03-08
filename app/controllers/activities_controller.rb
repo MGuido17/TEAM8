6c44fe53f5b963c7ecbfaf44a5cf4f40a03fb1e4
valeria lezcano <msvalerialg@gmail.com>
2024-03-08 11:01:40 +0100

    edit and destroy for create activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,7 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  before_action :check_authorization, only: [:edit, :update, :destroy]
+
   def new
     @activity = Activity.new
   end
@@ -18,6 +21,25 @@ class ActivitiesController < ApplicationController
     @activity = Activity.find(params[:id])
   end
 
+  def edit
+    @activity = current_user.activities.find(params[:id])
+  end
+
+  def update
+    @activity = current_user.activities.find(params[:id])
+    if @activity.update(activity_params)
+      redirect_to activity_path(@activity), notice: "The activity has successfully been updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @activity = current_user.activities.find(params[:id])
+    @activity.destroy
+    redirect_to activities_path, notice: "The activity has been deleted."
+  end
+
 private
 
   def activity_params
@@ -25,5 +47,13 @@ private
       whitelisted[:recommended_conditions] = params[:activity][:recommended_conditions].reject(&:blank?)
       whitelisted[:not_recommended_conditions] = params[:activity][:not_recommended_conditions].reject(&:blank?)
     end
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
+  def check_authorization
+    redirect_to root_path, alert: "Not authorized" unless @activity.organiser == current_user
   end
 end
