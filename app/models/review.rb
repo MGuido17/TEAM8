38f2f316c6205f8d30ab68c9158418ee7e66a754
valeria lezcano <msvalerialg@gmail.com>
2024-03-12 12:39:51 +0100

    started styling

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  validates :description, presence: true
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: 1..5 }
+
+
+
 end
