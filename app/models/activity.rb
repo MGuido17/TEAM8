51bedce183cc0ae42b07598e0ad4ed73dbe137c1
valeria lezcano <msvalerialg@gmail.com>
2024-03-12 09:20:44 +0100

    updated edit and update

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,7 +9,7 @@ class Activity < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :date, presence: true
-  validates :spaces, presence: true
+  validates :spaces, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :address, presence: true
   validates :location, presence: true
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,5 @@
 class Match < ApplicationRecord
   has_many :messages
-  has_many :match_user
+  has_many :match_users
+  has_many :users, through: :match_users
 end
